b5f6fbfc59aabf7bcd0d1456d1e5da0efd615054
bphillips-99 <83971940+bphillips-99@users.noreply.github.com>
2023-10-06 01:40:56 -0600

    import latest changes

---
+++ grpc_network_client/grpc_client.go
@@ -0,0 +1,86 @@
+package grpc_network_client
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"zera-network-grpc/zera_txn"
+	zera_validator "zera-network-grpc/zera_validator"
+)
+
+const GRPC_NETWORK_HIT_ADDRESS = "dev-validator-grpc.zera.vision:50051"
+
+// struct for client implementation of grpcs
+type ValidatorNetworkClient struct {
+	client zera_validator.ValidatorServiceClient
+}
+
+// struct for client implementation of grpcs
+type NetworkClient struct {
+	client zera_txn.TXNServiceClient
+}
+
+// constructor for client implementation of grpcs
+func NewNetworkClient(conn *grpc.ClientConn) *NetworkClient {
+	client := zera_txn.NewTXNServiceClient(conn)
+	return &NetworkClient{client: client}
+}
+
+// constructor for client implementation of grpcs
+func NewValidatorNetworkClient(conn *grpc.ClientConn) *ValidatorNetworkClient {
+	client := zera_validator.NewValidatorServiceClient(conn)
+	return &ValidatorNetworkClient{client: client}
+}
+
+// SendBlocksyncRequest sends a block sync request and returns the received block batch.
+func SendBlocksyncRequest(blockSync *zera_validator.BlockSync) (*zera_validator.BlockBatch, error) {
+	// Create a gRPC connection to the server
+	conn, err := grpc.Dial(GRPC_NETWORK_HIT_ADDRESS, grpc.WithInsecure()) // 137.184.181.31 dev // 146.190.163.205 proto // 146.190.139.36
+	if err != nil {
+		log.Fatalf("Failed to connect to the server: %v", err)
+		return nil, err
+	}
+	defer conn.Close()
+
+	// Create a new instance of ValidatorNetworkClient
+	client := NewValidatorNetworkClient(conn)
+
+	// Call the SyncBlockchain RPC
+	blockBatch, err := client.SyncBlockchain(context.Background(), blockSync)
+	if err != nil {
+		return blockBatch, nil
+	}
+
+	return blockBatch, nil
+}
+
+// SyncBlockchain sends a block sync request and returns the received block batch.
+func (v *ValidatorNetworkClient) SyncBlockchain(ctx context.Context, request *zera_validator.BlockSync) (*zera_validator.BlockBatch, error) {
+	response, err := v.client.SyncBlockchain(ctx, request)
+	return response, err
+}
+
+// SendMintTXN sends a mint txn and returns an empty response.
+func SendMintTXN(mintTXN *zera_txn.MintTXN) (*emptypb.Empty, error) {
+	// Create a gRPC connection to the server
+	conn, err := grpc.Dial("proto-txn-sender-v2.zera.vision:50052", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect to the server: %v", err)
+		return nil, err
+	}
+	defer conn.Close()
+
+	// Create a new instance of ValidatorNetworkClient
+	client := NewNetworkClient(conn)
+
+	response, err := client.client.Mint(context.Background(), mintTXN)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
---
+++ grpc_network_listener/grpc_listener.go
@@ -0,0 +1,115 @@
+package grpc_network_listener
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	zera_txn "zera-network-grpc/zera_txn"
+	zera_validator "zera-network-grpc/zera_validator"
+)
+
+// struct for server implementation of grpcs
+type ValidatorNetworkServer struct {
+	zera_validator.UnimplementedValidatorServiceServer //! TODO from network?
+}
+
+func StartgRPCService() {
+	// Create a gRPC server
+	server := grpc.NewServer()
+	validatorNetworkServer := &ValidatorNetworkServer{}
+
+	// Register the ValidatorNetworkServer with the gRPC server
+	zera_validator.RegisterValidatorServiceServer(server, validatorNetworkServer) //! TODO from network?
+
+	// Start listening on port 50051
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		fmt.Printf("Failed to listen: %v", err)
+	}
+
+	// Start the gRPC server
+	fmt.Println("Starting gRPC server on port 50051")
+	if err := server.Serve(listener); err != nil {
+		fmt.Printf("Failed to server: %v", err)
+	}
+
+}
+
+// listener service for broadcasts (new blocks)
+func (s *ValidatorNetworkServer) Broadcast(ctx context.Context, block *zera_validator.Block) (*emptypb.Empty, error) {
+
+	//TODO abstract to not take input method and not require db connection
+	// if block_processing_vars.IsBlockSyncActive() {
+	// 	return &emptypb.Empty{}, nil
+	// }
+
+	// // Block being sent from server doesn't contain hash (fixed in dev), so sync blocks for now // TODO network
+	// go block_processing_sync.DoBlockSync(database.GetEnv())
+
+	//go block_processing_insert.RecieveNewBlock()
+
+	return &emptypb.Empty{}, nil
+
+	// TODO implement when direct data is corrected
+
+	// Check if blocks are up to date
+	// if bytes.Compare(block.BlockHeader.GetPreviousBlockHash(), GetLastBlockHashByte(dbEnv)) != 0 && !isBlockSyncActive {
+	// 	fmt.Println("Behind Blocks, Syncing Blocks!")
+	// 	DoBlockSync(dbEnv)
+	// } else {
+	// 	// insert block
+	// 	fmt.Print("Block Recieved: ")
+	// 	fmt.Println(block.BlockHeader.GetBlockHeight())
+	// 	go InsertBlock(block, dbEnv)
+	// }
+
+	// return &emptypb.Empty{}, nil
+
+}
+
+// listener service for new validators
+func (s *ValidatorNetworkServer) ValidatorRegistration(ctx context.Context, valReg *zera_validator.ValidatorRegistrationMessage) (*emptypb.Empty, error) {
+	// TODO: Implement the logic for the ValidatorRegistration RPC
+	return &emptypb.Empty{}, nil
+}
+
+//TODO - May need to delete, dont think we need this functionality
+
+// func (s *ValidatorNetworkServer) SyncValidatorList(context.Context, *zera_validator.ValidatorSyncRequest) (*zera_validator.ValidatorSync, error) {
+// 	// TODO: Implement the logic for the SyncValidatorList RPC
+// 	return &zera_validator.ValidatorSync{}, nil
+// }
+
+// listener service for coin txns
+func (s *ValidatorNetworkServer) ValidatorCoin(ctx context.Context, txn *zera_txn.CoinTXN) (*emptypb.Empty, error) {
+	// TODO: Implement the logic for the ValidatorCoin RPC
+	return &emptypb.Empty{}, nil
+}
+
+//TODO - Will add this service when functionality is complete on validator side
+
+func (s *ValidatorNetworkServer) ValidatorMint(ctx context.Context, txn *zera_txn.MintTXN) (*emptypb.Empty, error) {
+	// TODO: Implement the logic for the ValidatorMint RPC
+	return &emptypb.Empty{}, nil
+}
+
+//TODO - Will add this service when functionality is complete on validator side
+
+func (s *ValidatorNetworkServer) ValidatorContract(ctx context.Context, txn *zera_txn.InstrumentContract) (*emptypb.Empty, error) {
+	// TODO: Implement the logic for the ValidatorContract RPC
+	return &emptypb.Empty{}, nil
+}
+
+func (s *ValidatorNetworkServer) ValidatorGovernProposal(ctx context.Context, txn *zera_txn.GovernanceProposal) (*emptypb.Empty, error) {
+	// TODO: Implement the logic for the ValidatorContract RPC
+	return &emptypb.Empty{}, nil
+}
+
+func (s *ValidatorNetworkServer) ValidatorGovernVoting(ctx context.Context, txn *zera_txn.GovernanceVote) (*emptypb.Empty, error) {
+	// TODO: Implement the logic for the ValidatorContract RPC
+	return &emptypb.Empty{}, nil
+}

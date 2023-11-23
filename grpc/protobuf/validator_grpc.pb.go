4a6e1420ec78f663caf915cdf9a83c086c4a361d
Will Tweed <100153629+WillGTw@users.noreply.github.com>
2023-11-23 10:28:20 -0700

    v0.0.76

--- grpc/protobuf/validator_grpc.pb.go
+++ grpc/protobuf/validator_grpc.pb.go
@@ -40,6 +40,8 @@ const (
 	ValidatorService_ValidatorContractUpdate_FullMethodName       = "/zera_validator.ValidatorService/ValidatorContractUpdate"
 	ValidatorService_ValidatorHeartbeat_FullMethodName            = "/zera_validator.ValidatorService/ValidatorHeartbeat"
 	ValidatorService_ValidatorFoundation_FullMethodName           = "/zera_validator.ValidatorService/ValidatorFoundation"
+	ValidatorService_ValidatorDelegatedVoting_FullMethodName      = "/zera_validator.ValidatorService/ValidatorDelegatedVoting"
+	ValidatorService_IndexerVoting_FullMethodName                 = "/zera_validator.ValidatorService/IndexerVoting"
 )
 
 // ValidatorServiceClient is the client API for ValidatorService service.
@@ -66,6 +68,8 @@ type ValidatorServiceClient interface {
 	ValidatorContractUpdate(ctx context.Context, in *ContractUpdateTXN, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	ValidatorHeartbeat(ctx context.Context, in *ValidatorHeartbeat, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	ValidatorFoundation(ctx context.Context, in *FoundationTXN, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	ValidatorDelegatedVoting(ctx context.Context, in *DelegatedVotingTXN, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	IndexerVoting(ctx context.Context, in *IndexerVotingReqest, opts ...grpc.CallOption) (*IndexerVotes, error)
 }
 
 type validatorServiceClient struct {
@@ -304,6 +308,24 @@ func (c *validatorServiceClient) ValidatorFoundation(ctx context.Context, in *Fo
 	return out, nil
 }
 
+func (c *validatorServiceClient) ValidatorDelegatedVoting(ctx context.Context, in *DelegatedVotingTXN, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, ValidatorService_ValidatorDelegatedVoting_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *validatorServiceClient) IndexerVoting(ctx context.Context, in *IndexerVotingReqest, opts ...grpc.CallOption) (*IndexerVotes, error) {
+	out := new(IndexerVotes)
+	err := c.cc.Invoke(ctx, ValidatorService_IndexerVoting_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ValidatorServiceServer is the server API for ValidatorService service.
 // All implementations must embed UnimplementedValidatorServiceServer
 // for forward compatibility
@@ -328,6 +350,8 @@ type ValidatorServiceServer interface {
 	ValidatorContractUpdate(context.Context, *ContractUpdateTXN) (*emptypb.Empty, error)
 	ValidatorHeartbeat(context.Context, *ValidatorHeartbeat) (*emptypb.Empty, error)
 	ValidatorFoundation(context.Context, *FoundationTXN) (*emptypb.Empty, error)
+	ValidatorDelegatedVoting(context.Context, *DelegatedVotingTXN) (*emptypb.Empty, error)
+	IndexerVoting(context.Context, *IndexerVotingReqest) (*IndexerVotes, error)
 	mustEmbedUnimplementedValidatorServiceServer()
 }
 
@@ -394,6 +418,12 @@ func (UnimplementedValidatorServiceServer) ValidatorHeartbeat(context.Context, *
 }
 func (UnimplementedValidatorServiceServer) ValidatorFoundation(context.Context, *FoundationTXN) (*emptypb.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ValidatorFoundation not implemented")
+}
+func (UnimplementedValidatorServiceServer) ValidatorDelegatedVoting(context.Context, *DelegatedVotingTXN) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ValidatorDelegatedVoting not implemented")
+}
+func (UnimplementedValidatorServiceServer) IndexerVoting(context.Context, *IndexerVotingReqest) (*IndexerVotes, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IndexerVoting not implemented")
 }
 func (UnimplementedValidatorServiceServer) mustEmbedUnimplementedValidatorServiceServer() {}
 
@@ -779,6 +809,42 @@ func _ValidatorService_ValidatorFoundation_Handler(srv interface{}, ctx context.
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ValidatorService_ValidatorDelegatedVoting_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DelegatedVotingTXN)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ValidatorServiceServer).ValidatorDelegatedVoting(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ValidatorService_ValidatorDelegatedVoting_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ValidatorServiceServer).ValidatorDelegatedVoting(ctx, req.(*DelegatedVotingTXN))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ValidatorService_IndexerVoting_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IndexerVotingReqest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ValidatorServiceServer).IndexerVoting(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ValidatorService_IndexerVoting_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ValidatorServiceServer).IndexerVoting(ctx, req.(*IndexerVotingReqest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ValidatorService_ServiceDesc is the grpc.ServiceDesc for ValidatorService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -857,6 +923,14 @@ var ValidatorService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ValidatorFoundation",
 			Handler:    _ValidatorService_ValidatorFoundation_Handler,
+		},
+		{
+			MethodName: "ValidatorDelegatedVoting",
+			Handler:    _ValidatorService_ValidatorDelegatedVoting_Handler,
+		},
+		{
+			MethodName: "IndexerVoting",
+			Handler:    _ValidatorService_IndexerVoting_Handler,
 		},
 	},
 	Streams: []grpc.StreamDesc{
